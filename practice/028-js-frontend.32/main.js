91d832f3e5180d216f734e1c5b8d679f72b8b9dd
huangmy <540759982@qq.com>
2018-12-04 17:08:08 +0800

    js基础练习

---
+++ practice/028-js-frontend.32/main.js
@@ -0,0 +1,47 @@
+console.log('yo,word agin');
+
+;(function(){
+    'use strict';
+
+})();
+
+;(function(){
+    'use strict';
+})();
+
+;(function(){
+    'use strict';
+})();
+
+;(function(){
+    'use strict';
+    console.log('yoyo');
+})();
+;(function(){
+    'use strict';
+    console.log('yoyoyoyoy');
+})();
+;(function(){
+    'use strict';
+    console.log('yoyoyoyoyo');
+})();
+;(function(){
+    'use strict';
+    console.log('yoyo');
+})();
+;(function(){
+    'use stirct';
+    console.log('yo');
+})();
+;(function(){
+    'use stirct';
+    console.log('yo');
+})();
+;(function(){
+    'use strict';
+    console.log('yo');
+})();
+;(function(){
+    'use strict';
+    console.log('yo');
+})();
